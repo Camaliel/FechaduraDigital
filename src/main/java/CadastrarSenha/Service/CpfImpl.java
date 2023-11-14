2def35eaa2260b3c3ac53d1d25cce2e527507459
Camaliel <83652580+Camaliel@users.noreply.github.com>
2023-11-14 17:32:59 -0300

    Implementado interface .. CPF

---
+++ src/main/java/CadastrarSenha/Service/CpfImpl.java
@@ -0,0 +1,11 @@
+package CadastrarSenha.Service;
+
+public interface CpfImpl {
+
+    /*
+     *INSERIR O CPF PASSANDO COMO PARAMETRO O NUMERO DO CPF COM 11 DIGITOS INCLUINDO O DIGITO SEM ESPAÇO ..
+     *
+     * */
+
+    String numeroCpf(String digiteCpf);
+}
