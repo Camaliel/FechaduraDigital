dd23c56a93088a001e48d683a3175912e8bf7ff0
Camaliel <83652580+Camaliel@users.noreply.github.com>
2023-11-28 12:53:23 -0300

    SenhaService ajustada

--- src/main/java/CadastrarSenha/Service/SenhaService.java
+++ src/main/java/CadastrarSenha/Service/SenhaService.java
@@ -1,29 +1,34 @@
 package CadastrarSenha.Service;
 
 import CadastrarSenha.Enum.MensagemSenhaEnum;
+import CadastrarSenha.Service.Interface.SenhaUsuarioImpl;
+import CadastrarSenha.Util.Variavel.InfoUsuario;
 
 import java.util.Scanner;
 
-public class SenhaService {
+public class SenhaService implements SenhaUsuarioImpl {
+    InfoUsuario usuario = new InfoUsuario();
 
-    public void cadastroSenha() {
+    public String cadastroSenha(String senha) {
+        String usuarioSenhaCadastrada = "";
+
+
+        System.out.println(MensagemSenhaEnum.CADASTRE_SENHA.getDescricao());
 
         Scanner leia = new Scanner(System.in);
-        System.out.println(MensagemSenhaEnum.CADASTRE_SENHA.getDescricao());
         String valor = leia.nextLine();
         while (valor.length() < 6) {
             if (valor.length() < 6 || valor.isBlank()) {
                 System.out.println(MensagemSenhaEnum.ERRO_SENHA.getDescricao());
                 valor = leia.nextLine();
+                usuarioSenhaCadastrada = usuario.setSenha(valor);
             }
         }
-            if (valor.length() == 6) {
-                System.out.println(MensagemSenhaEnum.SENHA_CADASTRADA.getDescricao());
-            }
-            leia.close();
+        if (valor.length() == 6) {
+            System.out.println(MensagemSenhaEnum.SENHA_CADASTRADA.getDescricao());
         }
-        public static void main (String[]args){
-            SenhaService senhaService = new SenhaService();
-            senhaService.cadastroSenha();
-        }
+        leia.close();
+        return usuarioSenhaCadastrada;
     }
+
+}
