bad5f25f5d05dd48554593f4e6f6035ce84d1168
Camaliel <83652580+Camaliel@users.noreply.github.com>
2023-11-28 12:51:04 -0300

    Menu process adicionado

--- src/main/java/CadastrarSenha/Processor/MenuProcessor.java
+++ src/main/java/CadastrarSenha/Processor/MenuProcessor.java
@@ -1,39 +1,61 @@
 package CadastrarSenha.Processor;
 
 import CadastrarSenha.Service.Interface.NumeroCelularImpl;
+import CadastrarSenha.Service.Interface.SenhaUsuarioImpl;
+import CadastrarSenha.Service.NumeroCelularService;
+import CadastrarSenha.Service.SenhaService;
 import CadastrarSenha.Util.Menu;
 import CadastrarSenha.Util.Variavel.InfoUsuario;
 import CadastrarSenha.Util.Variavel.VarFamiliar;
 
 
-public class MenuProcessor implements NumeroCelularImpl {
+public class MenuProcessor implements NumeroCelularImpl, SenhaUsuarioImpl {
     Menu menu = new Menu();
+    SenhaService senhaService = new SenhaService();
     VarFamiliar varFamiliar = new VarFamiliar();
     InfoUsuario usuario = new InfoUsuario();
+    NumeroCelularService numeroCelularService = new NumeroCelularService();
 
-    @Override
-    public String adicionaNumero(String numeroCelular) {
-         String numeroUsuario = usuario.getNumeroCelular();
+    String numeroUsuario = usuario.setNumeroCelular("");
 
-        return numeroUsuario;
-
-    }
+    String senhaUsuarioGravado = usuario.setSenha("999");
 
     public void menuPrincipal() {
         if (!menu.menuParente(varFamiliar.getValorMenu()).isBlank()) {
-
             System.out.println("proxima etapa");
-            System.out.println(adicionaNumero(usuario.getNumeroCelular()));
+            numeroCelularService.adicionaNumero(usuario.getNumeroCelular());
 
+            if (!numeroUsuario.isBlank()) {
+                System.out.println("proxima etapa");
+                cadastroSenha(senhaUsuarioGravado);
+                System.out.println("CADASTRO GRAVADO");
+            }
+            if (!cadastroSenha(senhaUsuarioGravado).isBlank()) {
+                System.out.println("PROXIMA ETAPA");
+
+            } else {
+                System.out.println("NAO FOI ");
+            }
 
         }
 
     }
 
+    @Override
+    public String adicionaNumero(String numeroCelular) {
+        return numeroUsuario;
+
+    }
+
+    @Override
+    public String cadastroSenha(String senha) {
+        return senhaUsuarioGravado;
+    }
 
     public static void main(String[] args) {
-
         MenuProcessor menu = new MenuProcessor();
         menu.menuPrincipal();
     }
+
+
 }
