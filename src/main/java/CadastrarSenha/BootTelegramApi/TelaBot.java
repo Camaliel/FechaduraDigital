732d033d897315bc1b1591e55bf23ec4cb24ef4c
Camaliel <83652580+Camaliel@users.noreply.github.com>
2024-01-19 23:30:37 -0300

    Ajuste da API Telegram

--- src/main/java/CadastrarSenha/BootTelegramApi/TelaBot.java
+++ src/main/java/CadastrarSenha/BootTelegramApi/TelaBot.java
@@ -2,6 +2,9 @@ package CadastrarSenha.BootTelegramApi;
 
 import CadastrarSenha.Repository.ConfereChaveToken;
 import CadastrarSenha.Repository.IncluiToken;
+import CadastrarSenha.Service.Interface.SenhaUsuarioImpl;
+import CadastrarSenha.Service.SenhaService;
+import CadastrarSenha.Util.Variavel.InfoUsuario;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -9,14 +12,20 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 import java.sql.SQLException;
 
 public class TelaBot {
+
+    SenhaService senhaService = new SenhaService();
+    InfoUsuario usuario = new InfoUsuario();
+    String senhaGravada = usuario.getSenha();
+    ConfereChaveToken confereChaveToken = new ConfereChaveToken();
+    IncluiToken incluiToken =  new IncluiToken();
     public void ligarApi() throws TelegramApiException, SQLException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         EcoBot bot = new EcoBot();
         telegramBotsApi.registerBot(new EcoBot());
         System.out.println("LIGADO");
-        IncluiToken incluiToken =  new IncluiToken();
+
         incluiToken.incluiToken();
-        ConfereChaveToken confereChaveToken = new ConfereChaveToken();
-        confereChaveToken.validaChaveToken();
+
     }
+
 }
