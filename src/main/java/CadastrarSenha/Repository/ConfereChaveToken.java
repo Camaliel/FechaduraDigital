e452263eba497136d6c39d03b8540d7988fcfd29
Camaliel <83652580+Camaliel@users.noreply.github.com>
2024-07-10 13:45:28 -0300

    🧹 cleanup: Limpeza de codigo

--- src/main/java/CadastrarSenha/Repository/ConfereChaveToken.java
+++ src/main/java/CadastrarSenha/Repository/ConfereChaveToken.java
@@ -1,9 +1,6 @@
 package CadastrarSenha.Repository;
 
-import CadastrarSenha.Service.ChaveToken;
-import CadastrarSenha.Util.Variavel.ValoresDigitados;
 import CadastrarSenha.jdbc.EnviaToken;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -11,11 +8,8 @@ import java.util.Scanner;
 //TODO ANALISAR PARA APAGAR A CLASSE !!! [LEMBRETE]
 public class ConfereChaveToken {
 
-    ValoresDigitados valoresDigitados = new ValoresDigitados();
-    ChaveToken chaveToken = new ChaveToken();
     EnviaToken token = new EnviaToken();
     IncluiToken incluiToken = new IncluiToken();
-    incluirPessoaRepository pessoaRepository = new incluirPessoaRepository();
 
     public String validaChaveToken() throws SQLException, ClassNotFoundException {
         Scanner leia = new Scanner(System.in);
