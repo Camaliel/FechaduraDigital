5aa791038d92e790abd1c0a59c43ebb13ee08b4b
Camaliel <83652580+Camaliel@users.noreply.github.com>
2024-08-13 09:55:50 -0300

    🧪 test: Criando novo teste

---
+++ src/test/java/CadastrarSenha/Repository/IncluiTokenTest.java
@@ -0,0 +1,43 @@
+package CadastrarSenha.Repository;
+
+import CadastrarSenha.Service.ChaveToken;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IncluiTokenTest {
+
+    private IncluiToken incluiToken;
+    private Connection conexaoMock;
+    private PreparedStatement statementMock;
+    private ChaveToken chaveToken;
+
+
+    @Test
+    public void deveRetornarQuery() throws SQLException {
+        conexaoMock = mock(Connection.class);
+        statementMock = mock(PreparedStatement.class);
+
+        // Mocke o método prepareStatement para retornar o PreparedStatement mockado
+        Mockito.when(conexaoMock.prepareStatement(any(String.class))).thenReturn(statementMock);
+
+
+
+       String teste = incluiToken.incluiToken();
+
+        assertEquals(incluiToken,teste);
+
+
+//        assertEquals("Numero enviado ==> " + new ChaveToken().getPegaToken(), tokenGerado);
+    }
+}
--- src/test/java/CadastrarSenha/jdbc/EnviaTokenTest.java
+++ src/test/java/CadastrarSenha/jdbc/EnviaTokenTest.java
@@ -2,7 +2,9 @@ package CadastrarSenha.jdbc;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.sql.*;
 
@@ -11,6 +13,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class EnviaTokenTest {
 
     private EnviaToken enviaToken;
