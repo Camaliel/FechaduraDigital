6748860af06d03a4849cf872a2351179bcddb1a3
Camaliel <83652580+Camaliel@users.noreply.github.com>
2025-01-03 15:38:18 -0300

    ✨ feat: Ajustado classe para criação de bancos e tabelas

--- src/main/java/CadastrarSenha/Repository/HistoricoRepository.java
+++ src/main/java/CadastrarSenha/Repository/HistoricoRepository.java
@@ -5,6 +5,8 @@ import CadastrarSenha.jdbc.DAO.Conexao;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static CadastrarSenha.Service.CpfService.cpfDigitado;
+
 public class HistoricoRepository {
     Conexao DAO = new Conexao();
     ArmazenaInformacaoPessoaRepository repository = new ArmazenaInformacaoPessoaRepository();
@@ -15,9 +17,10 @@ public class HistoricoRepository {
     public void enviaHistorico(String parentesco, String status) {
         String dataSistema = dataAtual.format(data);
         String horaSistema = horaAtual.format(data);
+        String cpf = cpfDigitado;
         // RECEBE DOIS PARAMETROS E ENVIA PARA O BANCO DEPENDENDO DO RESULTADO .. À ENTENDER ...
-        String sql = "INSERT INTO HISTORICO(DATA, HORA, PARENTESCO, STATUS) VALUES(?, ?, ?, ?)";
-        DAO.incluir(sql, dataSistema, horaSistema, parentesco, status);
+        String sql = "INSERT INTO moradores.historico (CPF,DATA, HORA, PARENTESCO, STATUS) VALUES(?, ?, ?, ?, ?)";
+        DAO.incluir(sql, cpf, dataSistema, horaSistema, parentesco, status);
     }
 
     public static void main(String[] args) {
--- src/main/java/CadastrarSenha/jdbc/BD/CriacaoDeBancosDAO.java
+++ src/main/java/CadastrarSenha/jdbc/BD/CriacaoDeBancosDAO.java
@@ -51,10 +51,10 @@ public class CriacaoDeBancosDAO {
             Statement statement = connection.createStatement();
             statement.execute(sql);
 
-            System.out.println("Banco de dados criado com SUCESSO");
+            System.out.println("Banco de dados [ADMINISTRADOR] criado com SUCESSO");
 
         } catch (SQLException e) {
-            System.out.println("Banco de dados já CONSTA no cadastro");
+            System.out.println("Banco de dados [ADMINISTRADOR] já CONSTA no cadastro");
         }
     }
 
@@ -63,20 +63,59 @@ public class CriacaoDeBancosDAO {
         try {
             Connection conexao = CriarConexao.getConnetion();
             String sql = " CREATE TABLE tokens (" +
-                    "id INT AUTO_INCREMENT, " +
-                    "nome VARCHAR(80)," +
-                    "campo INT," +
-                    "token VARCHAR(10)," +
-                    "PRIMARY KEY (nome)" +
+                    "parentesco VARCHAR(10)," +
+                    "token VARCHAR(10) UNIQUE, " +
+                    "chefe_familia VARCHAR(3)," +
+                    "nome varchar(15)," +
+                    "PRIMARY KEY (token)" +
                     ")";
 
             Statement stmt = conexao.createStatement();
             stmt.execute(sql);
 
-            System.out.println("Tabela criada com sucesso ...");
+            System.out.println("Tabela [TOKENS] criada com sucesso ...");
             conexao.close();
         } catch (SQLException e) {
-            System.out.println("Erro ao criar tabela");
+            System.out.println("Erro ao criar tabela [TOKENS]");
+        }
+    }
+    public void tabelaAdministrador() throws SQLException {
+        try {
+            Connection conexao = CriarConexao.getConnetion();
+            String sql = " CREATE TABLE administrador.tokens (" +
+                    "id INT AUTO_INCREMENT primary key," +
+                    "token VARCHAR(10) UNIQUE" +
+                    ");";
+
+            Statement stmt = conexao.createStatement();
+            stmt.execute(sql);
+
+            System.out.println("Tabela [ADMINISTRADOR] criada com sucesso ...");
+            conexao.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao criar tabela [ADMINISTRADOR]");
+        }
+    }
+    public void tbl_consulta() throws SQLException {
+        try {
+            Connection conexao = CriarConexao.getConnetion();
+            String sql ="CREATE TABLE tbl_consultas ( "+
+                    " cpf VARCHAR(11)," +
+                    "    nome VARCHAR(50)," +
+                    "    nome_do_meio VARCHAR(50)," +
+                    "    ultimo_nome VARCHAR(50)," +
+                    "    token VARCHAR(10) PRIMARY KEY," +
+                    "    CONSTRAINT fk_cpk FOREIGN KEY (cpf) REFERENCES cadastro (cpf)," +
+                    "    CONSTRAINT fk_token FOREIGN KEY (token) REFERENCES tokens (token) "+
+                    ")";
+
+            Statement stmt = conexao.createStatement();
+            stmt.execute(sql);
+
+            System.out.println("Tabela [CONSULTAS] criada com sucesso ...");
+            conexao.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao criar tabela [CONSULTAS]");
         }
     }
 
@@ -84,29 +123,52 @@ public class CriacaoDeBancosDAO {
         Connection conexao = CriarConexao.getConnetion();
         try {
         String sql = " CREATE TABLE cadastro (" +
-                "id INT AUTO_INCREMENT PRIMARY KEY, " +
                 "nome VARCHAR(80) NOT NULL," +
+                "nome_do_meio VARCHAR(80)," +
+                "ultimo_nome VARCHAR(80)," +
                 "chefe_familia VARCHAR(3)," +
                 "parentesco VARCHAR(10)," +
-                "cpf VARCHAR(12)," +
+                "cpf VARCHAR(12) primary key," +
                 "tel INT," +
                 "senha INT" +
                 ")";
-//nome, chefe_familia, parentesco,cpf, tel, senha
+
             Statement statement = conexao.createStatement();
             statement.execute(sql);
-            System.out.println("Tabela criada com sucesso ...");
+            System.out.println("Tabela [CADASTRO] criada com sucesso ...");
 
         } catch (SQLException e) {
-            System.out.println("Erro ao criar tabela");
+            System.out.println("Erro ao criar tabela [CADASTRO]");
+        }
+    }
+    public void tabelaHistorico() throws ClassNotFoundException, SQLException {
+        Connection conexao = CriarConexao.getConnetion();
+        try {
+            String sql = "CREATE TABLE historico ( " +
+                    "cpf varchar(11) primary key ," +
+                    "data varchar(10)," +
+                    "hora varchar(10)," +
+                    "parentesco varchar(10)," +
+                    "status varchar(10)," +
+                    "CONSTRAINT fp_cpk_historico FOREIGN KEY (cpf) REFERENCES cadastro (cpf)" +
+                    ");";
+
+            Statement statement = conexao.createStatement();
+            statement.execute(sql);
+            System.out.println("Tabela [HISTORICO] criada com sucesso ...");
+
+        } catch (SQLException e) {
+            System.out.println("Erro ao criar tabela [HISTORICO]");
         }
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         CriacaoDeBancosDAO criacaoDeBancosDAO = new CriacaoDeBancosDAO();
-//        criacaoDeBancos.CriarTabelaMoradores();
-//        criacaoDeBancos.tabelaCadastro();
+        criacaoDeBancosDAO.tabelaCadastro();
         criacaoDeBancosDAO.tabelaTokens();
-
+        criacaoDeBancosDAO.tbl_consulta();
+        criacaoDeBancosDAO.tabelaHistorico();
+        criacaoDeBancosDAO.criarTabelaAdministrador();
+        criacaoDeBancosDAO.tabelaAdministrador();
     }
 }
