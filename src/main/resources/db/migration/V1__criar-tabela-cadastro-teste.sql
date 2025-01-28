8ba08df47f7b1a83de1ac7877e718c1785fbe553
Camaliel <83652580+Camaliel@users.noreply.github.com>
2025-01-28 13:33:10 -0300

    ✨ feat: Implementação de flyway para versionamento do banco de testes

--- src/main/resources/db/migration/V1__criar-tabela-cadastro-teste.sql
+++ src/main/resources/db/migration/V1__criar-tabela-cadastro-teste.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE moradores-teste;
-CREATE DATABASE administrador-teste;
+CREATE DATABASE moradores_teste;
+CREATE DATABASE administrador_teste;
 
 CREATE TABLE cadastro-teste (
   nome VARCHAR(80) NOT NULL,
@@ -12,12 +12,12 @@ CREATE TABLE cadastro-teste (
   senha INT
 );
 
-CREATE TABLE moradores.tokens-teste (
+CREATE TABLE moradores_teste.tokens_teste (
  parentesco varchar(10),
- token VARCHAR(6) primary key,
+ token VARCHAR(6) NOT NULL primary key,
  chefe_familia VARCHAR(3),
  nome varchar(15)
- };
+);
 
 CREATE TABLE administrador.tokens-teste (
  id INT
@@ -27,15 +27,23 @@ CREATE TABLE administrador.tokens-teste (
  nome varchar(15)
 );
 
-CREATE TABLE moradores.tbl_consultas-teste (
+CREATE TABLE administrador_teste.tokens_teste (
+ id INT,
+ parentesco varchar(10),
+ token VARCHAR(6) primary key,
+ chefe_familia VARCHAR(3),
+ nome varchar(15)
+ );
+
+CREATE TABLE moradores_teste.tbl_consultas_teste (
  nome VARCHAR(50),
  nome_do_meio VARCHAR(50),
  ultimo_nome VARCHAR(50),
  token VARCHAR(6) PRIMARY KEY,
- CONSTRAINT fk_token FOREIGN KEY (token) REFERENCES tokens (token)
-);
+ CONSTRAINT fk_token FOREIGN KEY (token) REFERENCES moradores_teste.tokens_teste (token)
+ );
 
-CREATE TABLE cadastro-teste (
+ CREATE TABLE moradores_teste.cadastro_teste (
  nome VARCHAR(80) NOT NULL,
  nome_do_meio VARCHAR(80),
  ultimo_nome VARCHAR(80),
@@ -44,15 +52,15 @@ CREATE TABLE cadastro-teste (
  cpf VARCHAR(12) primary key,
  tel INT,
  senha INT
- );
+ )
 
-CREATE TABLE moradores.historico-teste (
- token varchar(6) primary key,
- nome varchar(50),
- nome_do_meio varchar(50),
- ultimo_nome varchar(50),
- data varchar(10),
- hora varchar(10),
- parentesco varchar(10),
- status varchar(10),
+ CREATE TABLE moradores_teste.historico_teste (
+  token varchar(6) primary key,
+  nome varchar(50),
+  nome_do_meio varchar(50),
+  ultimo_nome varchar(50),
+  data varchar(10),
+  hora varchar(10),
+  parentesco varchar(10),
+  status varchar(10)
  );
